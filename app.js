9c5e9fb6690d63a8ce8409152fc9dbb74bb79768
Brian Kimbrow <bkimbrow25@gmail.com>
2023-04-17 13:03:48 -0500

    New Update

--- app.js
+++ app.js
@@ -1,55 +1,42 @@
 "use strict"
-
-const { Button } = require("bootstrap");
-const { Alert } = require("bootstrap");
-
  
-  let answerOne =  prompt("Where do you live?").toLowerCase();
+let answerOne =  prompt("Do you live in Memphis?").toLowerCase();
 
-   if(answerOne === "memphis" || answerOne === "m") {
-    alert("I am from memphis")
-    
-   } else {
-    console.log("New York");
-   };
-
-   let answerTwo = prompt("How old are you?").toLowerCase();
-
-   if (answerTwo === 30 || answerTwo === "n") {
-    alert("We them boys");
-   } else {
-    console.log("We are not them");
-   }; 
-
-   let answerThree = prompt("What street you live on?").toLowerCase();
-
-   if(answerThree === "yale" && answerThree === 15 ) {
-    alert ("This is the street i live on")
-   } else{
-    console.log("Im now living in Nashville")
-   };
-
-   let answerFour = prompt("How long have you been here?").toLocaleLowerCase();
-
-   if(answerFour <= 10) {
-    alert("Thats not long")
-   } if (answerFour > 10 ) {
-    alert("How long you plan to stay?")
-   } else  {
-    alert("Im here to stay");
-   };
-
-   let answerFive = prompt("Have you completed the project").toLocaleLowerCase();
-
-   if (answerFive === "no" || answerFive === "n") {
-    alert("Are you trying to fail")
-  } if (answerFive === "yes" || answerFive === "y"){
-    alert("We are doing great!")
-} else {
-  alert("We are not doing great")
+ if(answerOne === "yes" || answerOne === "y") {
+  alert("I am from memphis")
+  
+ } else {
+  console.log("New York");
  };
 
- 
-   
+ let answerTwo = prompt("How old are you?").toLowerCase();
 
+ if (answerTwo === "no" || answerTwo === "n") {
+  alert("We them boys");
+ } else {
+  console.log("We are not them");
+ }; 
 
+ let answerThree = prompt("Do you go home often?").toLowerCase();
+
+ if(answerThree === "no" && answerThree === "n" ) {
+  alert ("This is the street i live on")
+ } else{
+  console.log("Im now living in Nashville")
+ };
+
+ let answerFour = prompt("Do you like living in nashville?").toLocaleLowerCase();
+
+ if(answerFour === "yes" || answerFour === "y") {
+  alert("Yes I like living in Nashville more than anything")
+ } else  {
+  console.log("Im here to stay");
+ };
+
+ let answerFive = prompt("Have you completed the project").toLocaleLowerCase();
+
+ if (answerFive === "no" || answerFive === "n") {
+  alert("Are you trying to fail")
+} else {
+alert("We are doing great")
+};
