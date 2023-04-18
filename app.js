dfe7e38ac1ebb9598fb131e4ab624401d32603e2
kimbrow1 <67285915+kimbrow1@users.noreply.github.com>
2023-04-18 12:30:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
 "use strict"
  
-  let answerOne =  prompt("Do you live in Memphis?").toLowerCase();
+let answerOne =  prompt("Do you live in Memphis?").toLowerCase();
 
-   if(answerOne === "yes" || answerOne === "y") {
-    alert("I am from memphis")
-    
-   } else {
-    console.log("New York");
-   };
-
-   let answerTwo = prompt("How old are you?").toLowerCase();
-
-   if (answerTwo === "no" || answerTwo === "n") {
-    alert("We them boys");
-   } else {
-    console.log("We are not them");
-   }; 
-
-   let answerThree = prompt("Do you go home often?").toLowerCase();
-
-   if(answerThree === "no" && answerThree === "n" ) {
-    alert ("This is the street i live on")
-   } else{
-    console.log("Im now living in Nashville")
-   };
-
-   let answerFour = prompt("Do you like living in nashville?").toLocaleLowerCase();
-
-   if(answerFour === "yes" || answerFour === "y") {
-    alert("Yes I like living in Nashville more than anything")
-   } else  {
-    console.log("Im here to stay");
-   };
-
-   let answerFive = prompt("Have you completed the project").toLocaleLowerCase();
-
-   if (answerFive === "no" || answerFive === "n") {
-    alert("Are you trying to fail")
-  } else {
-  alert("We are doing great")
+ if(answerOne === "yes" || answerOne === "y") {
+  alert("I am from memphis")
+  
+ } else {
+  console.log("New York");
  };
+
+ let answerTwo = prompt("How old are you?").toLowerCase();
+
+ if (answerTwo === "no" || answerTwo === "n") {
+  alert("We them boys");
+ } else {
+  console.log("We are not them");
+ }; 
+
+ let answerThree = prompt("Do you go home often?").toLowerCase();
+
+ if(answerThree === "no" && answerThree === "n" ) {
+  alert ("This is the street i live on")
+ } else{
+  console.log("Im now living in Nashville")
+ };
+
+ let answerFour = prompt("Is football your favorite sport?").toLocaleLowerCase();
+
+ if(answerFour === "yes" || answerFour === "y") {
+  alert("Yes football is great")
+ } else  {
+  console.log("They prefer basketball");
+ };
+
+ let answerFive = prompt("Have you completed the project").toLocaleLowerCase();
+
+ if (answerFive === "no" || answerFive === "n") {
+  alert("Are you trying to fail")
+} else {
+alert("We are doing great")
+};
