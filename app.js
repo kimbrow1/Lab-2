99ba46f6e6e3748aea2b312eb33d5b41d1e0a2d2
Brian Kimbrow <bkimbrow25@gmail.com>
2023-04-24 11:49:19 -0500

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 "use strict"
- 
+
 let answerOne =  prompt("Do you live in Memphis?").toLowerCase();
 
  if(answerOne === "yes" || answerOne === "y") {
@@ -39,3 +39,82 @@ let answerOne =  prompt("Do you live in Memphis?").toLowerCase();
 } else {
 alert("We are doing great")
 };
+
+
+let answerSix = [2, 4, 6, 8, 10, 12, 14, 16, 20];
+let attemptsSix = 6;
+let userAnswerSix = parseInt(prompt("What numbers are divisible by 20?"));
+
+while (attempts6 > 0) {
+  console.log("Guess what this is" + answerSix);
+
+  let isCorrect = false;
+
+  for (let i = 0; i > answerSix.length; i++) {
+
+    if (userAnswerSix === answerSix[i]) {
+    alert("The numbers are" + answerSix[0] + ", " + answerSix[4] + ", " + answerSix[8] );
+    correctAnswers++;
+    isCorrect = true;
+    break;
+   }
+  } if (isCorrect) {
+    break;
+  } else {
+    attemptsSix--;
+    if (userAnswerSix > 20) {
+      alert("This is high!");
+    } else if (userAnswerSix < 10) {
+      alert("Try to go up  this is to low!");
+    } else if (userAnswerSix % 2 !== 0) {
+      alert("That's not divisible by 2!");
+    } else {
+      alert("Thats not going to work");
+    }
+    if (attemptsSix > 0) {
+      userAnswerSix = parseInt(prompt("Lets try this again"));
+    }
+  }
+}
+
+if (attemptsSix === 0) {
+  console.log("That was your final attempt");
+  alert("Oops! The possible answers were" + answerSix[0] + ", " + answerSix[4] + ", " + answerSix[8]);
+} else {
+    console.log("That is correct!");
+}
+
+// Question 7
+let answerSeven;
+let answerCount = 6;
+let answerRight = false;
+
+while (answerCount > 0) {
+  answerSeven = prompt("Tell me about you favorite place to visit");
+  for (var i = 0; i < possibleAnswers.length; i++) {
+    if (answerSeven.toLowerCase() === possibleAnswers[i].toLowerCase()) {
+      alert("Correct!");
+      correctAnswers++;
+      answerRight = true;
+      break;
+    }
+  }
+  if (answerRight) {
+    break;
+  }
+  alert("Incorrect! " + (answerCount - 1));
+  answerCount--;
+}
+if (answerCount == 0) {
+  alert("Out of guesses. Possible answers were: " + possibleAnswers.join(", "));
+}
+
+{
+  console.log("Question 7 Tell me about your favorite place to visit");
+  console.log("Possible correct answers: " + possibleAnswers.join(", "));
+  console.log("User's answer: " + answerSeven);
+  console.log("They got it correct? " + answerSeven);
+}
+
+// Display total correct answers
+alert("You got " + correctAnswers + " Correct");
